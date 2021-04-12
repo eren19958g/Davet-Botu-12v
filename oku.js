668de801231bdefcd528aa93e02b03415adae2ea
eren19958g <78706192+eren19958g@users.noreply.github.com>
2021-04-12 17:20:35 +0300

    Create oku.js

---
+++ oku.js
@@ -0,0 +1,60 @@
+
+// Main İçine Bunu At
+client.on("ready", () => {
+  wait(1000);
+
+  client.guilds.cache.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on("guildMemberRemove", async member => {
+  let kanal = await db.fetch(`davetkanal_${member.guild.id}`);
+  if (!kanal) return;
+  let veri = await db.fetch(`rol1_${member.guild.id}`);
+  let veri12 = await db.fetch(`roldavet1_${member.guild.id}`);
+  let veri21 = await db.fetch(`roldavet2_${member.guild.id}`);
+  let veri2 = await db.fetch(`rol2_${member.guild.id}`);
+  let d = await db.fetch(`bunudavet_${member.id}`);
+  const sa = client.users.cache.get(d);
+  const sasad = member.guild.members.cache.get(d);
+  let sayı2 = await db.fetch(`davet_${d}_${member.guild.id}`);
+  db.add(`davet_${d}_${member.guild.id}`, -1);
+
+  if (!d) {
+    const aa = new Discord.MessageEmbed()
+      .setColor("BLACK")
+      .setDescription(
+        `\`\`${member.user.tag}\`\` **adlı şahıs aramızdan ayrıldı.\nŞahsı davet eden:** \`\`Bulunamadı!\`\``
+      )
+      .setFooter(client.user.username, client.user.avatarURL());
+    client.channels.cache.get(kanal).send(`**${member.user.tag}** adlı kullanıcı aramızdan ayrıldı.\nKullanıcıyı davet eden: **Bulunamadı!**`);
+    return;
+  } else {
+    const aa = new Discord.MessageEmbed()
+      .setColor("BLACK")
+      .setDescription(
+        `\`\`${member.user.tag}\`\` **adlı şahıs aramızdan ayrıldı.\nŞahsı davet eden:** \`\`${sa.tag}\`\``
+      )
+      .setFooter(client.user.username, client.user.avatarURL());
+    client.channels.cache.get(kanal).send(`**${member.user.tag}** adlı kullanıcı aramızdan ayrıldı.\nKullanıcıyı davet eden: **${sa.tag}**`);
+
+    if (!veri) return;
+
+    if (sasad.roles.cache.has(veri)) {
+      if (sayı2 <= veri12) {
+        sasad.roles.remove(veri);
+        return;
+      }
+    }
+    if (sasad.roles.cache.has(veri2)) {
+      if (!veri2) return;
+      if (sayı2 <= veri21) {
+        sasad.roles.remove(veri2);
+        return;
+      }
+    }
+  }
+});
